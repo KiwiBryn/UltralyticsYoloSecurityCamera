62fe7b7e67eb2250ffcb5e01dc60646eb5830a84
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2024-12-31 17:32:17 +1300

    Start of YoloDotNet configuration settings

--- YoloDotNetRTSPCameraNagerVideoStream/Model.cs
+++ YoloDotNetRTSPCameraNagerVideoStream/Model.cs
@@ -13,5 +13,10 @@ namespace devMobile.IoT.Ultralytics.YoloDotNetRtspCamera.NagerVideoStream.Model
 		public string RtspCameraUrl { get; set; }
 
       public string ImageFilepathLocal { get; set; }
-	}
+
+      public required string ModelPath { get; set; }
+      public required bool CUDA { get; set; } = false;
+      public required int GPUId { get; set; } = 0;
+      public required bool PrimeGPU { get; set; } = false;
+   }
 }
