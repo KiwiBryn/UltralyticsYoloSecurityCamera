f7ecfa8509ce05b18fa0b5754146a04e5fabb3bb
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2025-01-06 12:41:41 +1300

    Sttubed out NagerVideoStream investigation

---
+++ RTSPCameraNagerVideoStream/Program.cs
@@ -0,0 +1,19 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) January 2025, devMobile Software
+//
+// https://mit-license.org/
+//
+// Big thanks https://github.com/nager/Nager.VideoStream
+//
+//---------------------------------------------------------------------------------
+
+namespace RTSPCameraNagerVideoStream
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello, World!");
+      }
+   }
+}
