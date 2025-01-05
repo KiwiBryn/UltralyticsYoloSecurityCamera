6ac61452c01dd1a4fec3d397ae22f13f10ea882b
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2025-01-06 12:53:43 +1300

    Stubbed out SharpRTSP client for evualtion of performance

---
+++ RTSPCameraSharpRTSPClient/Program.cs
@@ -0,0 +1,18 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) January 2025, devMobile Software
+//
+// https://mit-license.org/
+//
+// Big thanks https://github.com/ngraziano/SharpRTSP
+//
+//---------------------------------------------------------------------------------
+namespace RTSPCameraSharpRTSPClient
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello, World!");
+      }
+   }
+}
