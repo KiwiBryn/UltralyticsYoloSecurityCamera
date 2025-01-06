cb2da342e2e18a2216101988f299655a21334b80
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2025-01-06 14:12:17 +1300

    First cut of Nager VideoStream test application

---
+++ RTSPCameraNagerVideoStream/Model.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) January 2025, devMobile Software
+//
+// https://mit-license.org/
+//
+// Thanks https://github.com/nager/Nager.VideoStream
+//
+//---------------------------------------------------------------------------------
+namespace devMobile.IoT.RTSPCameraNagerVideoStream.Model
+{
+	public class ApplicationSettings
+	{
+      public string RtspCameraUrl { get; set; } = "";
+
+      public string ImageFilepathLocal { get; set; } = "Images";
+   }
+}
--- RTSPCameraNagerVideoStream/Program.cs
+++ RTSPCameraNagerVideoStream/Program.cs
@@ -6,14 +6,99 @@
 // Big thanks https://github.com/nager/Nager.VideoStream
 //
 //---------------------------------------------------------------------------------
+using Microsoft.Extensions.Configuration;
 
-namespace RTSPCameraNagerVideoStream
+using Nager.VideoStream;
+
+
+namespace devMobile.IoT.RTSPCameraNagerVideoStream
 {
-   internal class Program
+   class Program
    {
+      private static Model.ApplicationSettings _applicationSettings;
+
       static void Main(string[] args)
       {
-         Console.WriteLine("Hello, World!");
+         Console.WriteLine($"{DateTime.UtcNow:yy-MM-dd HH:mm:ss} devMobile.IoT.RTSPCameraNagerVideoStream starting");
+#if RELEASE
+         Console.WriteLine("RELEASE");
+#else
+         Console.WriteLine("DEBUG");
+#endif
+         try
+         {
+            // load the app settings into configuration
+            var configuration = new ConfigurationBuilder()
+                 .AddJsonFile("appsettings.json", false, true)
+                 .AddUserSecrets<Program>()
+            .Build();
+
+            _applicationSettings = configuration.GetSection("ApplicationSettings").Get<Model.ApplicationSettings>();
+
+            if (!Directory.Exists(_applicationSettings.ImageFilepathLocal))
+            {
+               Directory.CreateDirectory(_applicationSettings.ImageFilepathLocal);
+            }
+
+            var inputSource = new StreamInputSource(_applicationSettings.RtspCameraUrl);
+
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            _ = Task.Run(async () => await StartStreamProcessingAsync(inputSource, cancellationTokenSource.Token));
+
+            Console.WriteLine("Press any key to stop");
+            Console.ReadKey();
+
+            cancellationTokenSource.Cancel();
+         }
+         catch (Exception ex)
+         {
+            Console.WriteLine($"{DateTime.UtcNow:yy-MM-dd HH:mm:ss} Application shutdown failure {ex.Message}", ex);
+         }
+
+         Console.WriteLine("Press ENTER to exit");
+         Console.ReadLine();
       }
+
+      private static async Task StartStreamProcessingAsync(InputSource inputSource, CancellationToken cancellationToken = default)
+      {
+         Console.WriteLine("Start Stream Processing");
+         try
+         {
+            var client = new VideoStreamClient();
+
+            client.NewImageReceived += NewImageReceived;
+#if FFMPEG_INFO_DISPLAY
+            client.FFmpegInfoReceived += FFmpegInfoReceived;
+#endif
+            File.WriteAllText(Path.Combine(_applicationSettings.ImageFilepathLocal, $"{DateTime.UtcNow:yyyyMMdd-HHmmss.fff}.txt"), "Start");
+
+            await client.StartFrameReaderAsync(inputSource, OutputImageFormat.Png, cancellationToken: cancellationToken);
+
+            File.WriteAllText(Path.Combine(_applicationSettings.ImageFilepathLocal, $"{DateTime.UtcNow:yyyyMMdd-HHmmss.fff}.txt"), "Finish");
+
+            client.NewImageReceived -= NewImageReceived;
+#if FFMPEG_INFO_DISPLAY
+            client.FFmpegInfoReceived -= FFmpegInfoReceived;
+#endif
+            Console.WriteLine("End Stream Processing");
+         }
+         catch (Exception exception)
+         {
+            Console.WriteLine($"{exception}");
+         }
+      }
+
+      private static void NewImageReceived(byte[] imageData)
+      {
+         File.WriteAllBytes( Path.Combine(_applicationSettings.ImageFilepathLocal, $"{DateTime.UtcNow:yyyyMMdd-HHmmss.fff}.png"), imageData);
+      }
+
+#if FFMPEG_INFO_DISPLAY
+      private static void FFmpegInfoReceived(string ffmpegStreamInfo)
+      {
+         Console.WriteLine(ffmpegStreamInfo);
+      }
+#endif
    }
 }
