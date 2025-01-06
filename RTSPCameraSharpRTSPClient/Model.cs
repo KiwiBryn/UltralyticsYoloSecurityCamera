6a4c88875839cc1ce1f3c8d0df793d18572c3410
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2025-01-07 10:09:16 +1300

    Added model so user secrets

---
+++ RTSPCameraSharpRTSPClient/Model.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) December 2024, devMobile Software
+//
+// https://www.gnu.org/licenses/#AGPL
+//
+// Thanks https://github.com/nager/Nager.VideoStream
+//
+//---------------------------------------------------------------------------------
+namespace devMobile.IoT.RTSPCameraSharpRTSP.Model
+{
+   public class ApplicationSettings
+   {
+      public string RtspCameraUrl { get; set; } = "";
+
+      public string ImageFilepathLocal { get; set; } = "Images";
+
+      public string CameraUserName { get; set; } = "";
+
+      public string CameraPassword { get; set; } = "";
+   }
+}
