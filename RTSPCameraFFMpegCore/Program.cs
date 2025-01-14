125927de2be96b16ff212f1d86ede990d689155f
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2025-01-14 15:06:13 +1300

    First cut of FFMpegCore implementaion.
    Stubbed out project in solution and merged into main so less painful in future

---
+++ RTSPCameraFFMpegCore/Program.cs
@@ -0,0 +1,18 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) January 2025, devMobile Software
+//
+// https://mit-license.org/
+//
+// Thanks https://github.com/rosenbjerg/FFMpegCore/
+//
+//---------------------------------------------------------------------------------
+namespace devMobile.IoT.RTSPCameraFFMpegCore
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello, World!");
+      }
+   }
+}
